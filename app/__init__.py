c7e57756d3467fad7d06c025abe6ca3f6042c8fa
denisseai <msdenisseanaya@gmail.com>
2021-07-12 16:02:58 -0700

    inital commit

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
         "SQLALCHEMY_DATABASE_URI")
 
+
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
 
@@ -24,8 +25,14 @@ def create_app():
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    # from .routes import example_bp
-    # app.register_blueprint(example_bp)
+    from app.models.card import Card
+    from app.models.board import Board
+
+    from .routes import card_bp
+    app.register_blueprint(card_bp)
+
+    from .routes import board_bp
+    app.register_blueprint(board_bp)
 
     CORS(app)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,11 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
+from app.models.board import Board
 
-# example_bp = Blueprint('example_bp', __name__)
+card_bp = Blueprint('card_bp', __name__)
+board_bp = Blueprint('board_bp', __name__)
+
+@board_bp.route('/', methods=["GET"])
+def root():
+    return {"name":"soemthing"}
