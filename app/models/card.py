a6a788c5604c335f799ba2f7481c7f4741badfe7
denisseai <msdenisseanaya@gmail.com>
2021-07-13 14:38:29 -0700

    Added Board CRUD and Card create & read
    Co-authored-by: Naomi Quinones <naomiquinones@users.noreply.github.com>
    Co-authored-by: Risha <rishallen@users.noreply.github.com>

--- app/models/card.py
+++ app/models/card.py
@@ -3,4 +3,5 @@ from app import db
 class Card (db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
-    like_count = db.Column(db.Integer)
+    like_count = db.Column(db.Integer, default=0)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=False)
